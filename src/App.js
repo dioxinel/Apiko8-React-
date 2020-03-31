ffaf1b9a083b5936c16f66409693d290d302ce21
Andriy <dioxine2000@gmail.com>
2020-03-31 12:00:23 +0300

    Fix bug in pagination

--- src/App.js
+++ src/App.js
@@ -323,31 +323,51 @@ class Pagination extends React.Component {
         if (!node) {
             return;
           }
-        this.setState({center: Number(e.target.innerHTML)})
+    
+         const center = Number(e.target.innerHTML);
+
+        this.setState({center})
         e.target.classList.add('ClickedPage');
         this.props.onPageClick();
     }
     render() {
         const list = [];
-        
-        let center = this.state.center; 
+        let offsetIndex = 4;
+        let center = this.state.center;
         const lastPage = this.props.numOfPagesInList
-        if (center < 6) {
-            center = 6;
+        
+        if (lastPage === 1) {
+            return (<p hidden='true'>C</p>)
         }
-        if (center > lastPage - 4) {
-            center = lastPage - 4;
+        if(lastPage < 9) {
+            for (let idx = 1; idx < lastPage; idx++ ){
+                list.push(<Page num={idx} key={idx} />);
+            }
+            return (
+                <div onClick={this.handlePageClick}>
+                    {list}
+                </div>
+            )
+        }
+
+        if (center < 5) {
+            center = 5;
+        }
+
+        if (center > lastPage - 3) {
+            center = lastPage - 3;
         } 
-        if(center > 6) {
+
+        if(center > 5) {
             list.push(<Page num={1} key={1} />)
             list.push(<p className='page' key='after'>  </p>);
         }
 
-        for (let idx = center - 5; idx < center + 5; idx++ ){
+        for (let idx = center - offsetIndex; idx < center + offsetIndex; idx++ ){
             list.push(<Page num={idx} key={idx} />);
         }
 
-        if (center < lastPage - 6) {
+        if (center < lastPage - 3) {
             list.push(<p className='page' key='before'>  </p>);
             list.push(<Page num={lastPage} key={lastPage} />)
         }
@@ -405,22 +425,20 @@ class VideoContainer extends React.Component {
     render() {
         const displayedTrailer = this.state.displayedTrailer;
         const numOfTrailers = this.props.videoData.results.length;
-
         if (!numOfTrailers) {
             return (
                 <h3>This title don't have trailer.</h3>
             )
         }
-
         if (numOfTrailers === 1) {
             return (
                 <div>
-                    <h3>Trailer</h3>
-                    <VideoComponent videoData={this.props.videoData} displayedTrailer={displayedTrailer} />
+                <h3>Trailer</h3>
+                <VideoComponent videoData={this.props.videoData} displayedTrailer={displayedTrailer} />
                 </div>
             )
         }
-        
+
         return (
             <div>
                 <h3>Trailer</h3>
