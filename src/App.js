0d84b676d82d7745e416456a76f78865be678e17
Andriy <dioxine2000@gmail.com>
2020-03-31 09:52:27 +0300

    Update trailers functionality

--- src/App.js
+++ src/App.js
@@ -218,7 +218,7 @@ class TitleDiscription extends React.Component {
                titlesList={titlesList}
                onTitleClick={this.props.onTitleClick} 
                />
-               <VideoDis videoData={titleData.videoData} />
+               <VideoContainer videoData={titleData.videoData} />
             </div>
         )
     }
@@ -369,13 +369,77 @@ class Page extends React.Component {
 }
 
 
-class VideoDis extends React.Component {
+class VideoContainer extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            displayedTrailer: 1
+        }
+        this.handleTrailerPreviousClick = this.handleTrailerPreviousClick.bind(this);
+        this.handleTrailerNextClick = this.handleTrailerNextClick.bind(this);
+    }
+
+
+    handleTrailerPreviousClick() {
+        let displayedTrailer;
+        if (this.state.displayedTrailer - 1 < 1) {
+            displayedTrailer = this.props.videoData.results.length;
+        } else {
+            displayedTrailer = this.state.displayedTrailer - 1;
+        }
+        this.setState({displayedTrailer})
+    }
+
+
+    handleTrailerNextClick() {
+        let displayedTrailer;
+        if (this.state.displayedTrailer + 1 > this.props.videoData.results.length) {
+            displayedTrailer = 1;
+        } else {
+            displayedTrailer = this.state.displayedTrailer + 1;
+        }
+        this.setState({displayedTrailer})
+    }
+
+
     render() {
-        const link = `https://www.youtube.com/embed/${this.props.videoData.results[0].key}`
+        const displayedTrailer = this.state.displayedTrailer;
+        const numOfTrailers = this.props.videoData.results.length;
+
+        if (!numOfTrailers) {
+            return (
+                <h3>This title don't have trailer.</h3>
+            )
+        }
+
+        if (numOfTrailers === 1) {
+            return (
+                <div>
+                    <h3>Trailer</h3>
+                    <VideoComponent videoData={this.props.videoData} displayedTrailer={displayedTrailer} />
+                </div>
+            )
+        }
+        
         return (
             <div>
                 <h3>Trailer</h3>
-                <iframe 
+                <p>There is: {numOfTrailers} trailers, you watch {displayedTrailer}</p>
+                <VideoComponent videoData={this.props.videoData} displayedTrailer={displayedTrailer} />
+                <button onClick={this.handleTrailerPreviousClick}>Previous</button>
+                <button onClick={this.handleTrailerNextClick}>Next</button>
+            </div>
+        ) 
+    }
+}
+
+
+class VideoComponent extends React.Component {
+    render() {
+        const link = `https://www.youtube.com/embed/${this.props.videoData.results[this.props.displayedTrailer - 1].key}`;
+
+        return (
+            <iframe 
                     width="560" 
                     height="315" 
                     src={link} 
@@ -386,8 +450,8 @@ class VideoDis extends React.Component {
                     picture-in-picture" 
                     allowFullScreen
                     title='Trailer'></iframe>
-            </div>
         )
     }
 }
+
 export default App;
